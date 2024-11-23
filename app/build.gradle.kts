3c950753a33f4d929d6ce960afa2860a8eab827a
Brent Tunnicliff <brent@tunnicliff.dev>
2024-11-24 01:45:57 +1100

    Bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "dev.tunnicliff.dadjokes"
         minSdk = 33
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0-beta.2"
+        versionCode = 2
+        versionName = "1.0.0-beta"
 
         buildConfigField(
             "String",
